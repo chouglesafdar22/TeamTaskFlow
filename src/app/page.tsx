23d0869b48ad011980d1d55f7914fc90a1931819
Safdar Chougle <chouglesafdar22@gmail.com>
2025-12-08 21:53:02 +0530

    created hero section & feature card on main page 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
       <Navbar />
 
       <section className="header xl:pt-40 lg:pt-36 md:pt-32 sm:pt-28 pt-24 xl:pb-20 lg:pb-16 md:py-14 sm:py-12 py-11 xl:px-6 lg:px-5 md:px-4 sm:px-3 px-1.5">
-        <div className="flex flex-col justify-center container items-center gap-4 py-2.5 md:px-5 px-2.5 mx-auto">
+        <div className="flex flex-col justify-center container gap-4 py-2.5 md:px-5 px-2.5 mx-auto">
           <h2 className="text-white text-center poppins-semibold xl:text-3xl lg:text-2xl md:text-xl base:text-lg text-sm">GemAi — Your Ultimate Personal AI Assistant</h2>
           <span className="text-white md:text-center text-left line-clamp-4 poppins-regular xl:text-xl lg:text-lg md:text-base base:text-sm text-xs wrap-break-word">GemAi empowers you with the intelligence of Google’s Gemini API, delivering fast, accurate, and deeply intuitive responses to everything you need. Write, code, learn, analyze, and create effortlessly with an AI assistant designed to boost your productivity and help you achieve more every single day.
           </span>
         </div>
       </section>
 
-      <section className="features xl:px-6 lg:px-5 md:px-4 sm:px-3 px-1.5">
+      <section className="features xl:pt-5 lg:pt-4 md:pt-3 sm:pt-2 pt-1.5 xl:px-6 lg:px-5 md:px-4 sm:px-3 px-1.5">
         <div className="flex flex-col sm:gap-8 gap-5 xl:justify-center py-2.5 md:px-5 px-2.5">
           <h3 className="text-white text-center poppins-medium xl:text-3xl lg:text-2xl md:text-xl sm:text-lg text-base">Features</h3>
           <div className="flex sm:flex-row flex-col justify-center items-center gap-5">
