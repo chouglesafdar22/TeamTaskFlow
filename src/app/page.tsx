f5d27659ad2a6947dc5d3a09bff00e2bec8d4d5b
Safdar Chougle <chouglesafdar22@gmail.com>
2025-12-08 21:50:41 +0530

    created hero section & feature card on main page 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
       <section className="features xl:px-6 lg:px-5 md:px-4 sm:px-3 px-1.5">
         <div className="flex flex-col sm:gap-8 gap-5 xl:justify-center py-2.5 md:px-5 px-2.5">
-          <h3 className="text-white text-center poppins-medium xl:text-3xl lg:text-2xl md:text-xl base:text-lg text-sm">Features</h3>
+          <h3 className="text-white text-center poppins-medium xl:text-3xl lg:text-2xl md:text-xl sm:text-lg text-base">Features</h3>
           <div className="flex sm:flex-row flex-col justify-center items-center gap-5">
             <FeatureCard title={"Smart Responses Powered"} text={"Get fast, accurate, and deeply contextual answers for anything you ask. GemAi uses the Gemini API to deliver intelligent results instantly."} />
             <FeatureCard title={"Write & Create Effortlessly"} text={"From emails and blogs to ideas and summaries — GemAi helps you write and create high-quality content in seconds."} />
